9fb9de70ae201723df8e9ce65d87b93df8f8439b
Tomas Ezequiel Modia <107971933+TomasM03@users.noreply.github.com>
2024-11-22 15:04:56 -0300

    fixes

--- Assets/Scripts/Canvas/SceneManagerButtons.cs
+++ Assets/Scripts/Canvas/SceneManagerButtons.cs
@@ -64,6 +64,26 @@ public class SceneManagerButtons : MonoBehaviour
     {
         SceneManager.LoadScene("Level 4");
     }
+    public void Level5()
+    {
+        SceneManager.LoadScene("Level 5");
+    }
+    public void Level6()
+    {
+        SceneManager.LoadScene("Level 6");
+    }
+    public void Level7()
+    {
+        SceneManager.LoadScene("Level 7");
+    }
+    public void Level8()
+    {
+        SceneManager.LoadScene("Level 8");
+    }
+    public void Level9()
+    {
+        SceneManager.LoadScene("Level 9");
+    }
 
     //Credits Button
     public void Credits()
